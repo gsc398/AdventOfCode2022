04da35797f21e782f8e1539598f6afafb37c2daa
Tamas Kozma <tamas-kozma@outlook.com>
2023-01-07 20:52:38 +0100

    helo

---
+++ Advent of Code 2022/Day01CalorieCounting/Program.cs
@@ -0,0 +1,29 @@
+﻿var sorok = File.ReadAllLines("example.txt");
+
+var szamok = new List<int>();
+foreach (var sor in sorok)
+{
+    if (sor.Length == 0)
+    {
+        continue;
+    }
+    int szam = int.Parse(sor);
+    szamok.Add(szam);
+}
+
+
+/*
+var lista = new List<string>();
+lista.Add("Hello");
+lista.Add("Jani");
+
+foreach (string elem in lista)
+{
+    Console.WriteLine(elem);
+}
+
+for (int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine(lista[i]);
+}
+*/
