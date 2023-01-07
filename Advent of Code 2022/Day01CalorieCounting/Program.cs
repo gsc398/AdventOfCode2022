e3c3895f653b886ef0fbbe2a8005ec0a95fdcac0
Tamas Kozma <tamas-kozma@outlook.com>
2023-01-07 23:14:01 +0100

    Day1

--- Advent of Code 2022/Day01CalorieCounting/Program.cs
+++ Advent of Code 2022/Day01CalorieCounting/Program.cs
@@ -1,7 +1,7 @@
-﻿var sorok = File.ReadAllLines("example.txt");
+﻿var sorok = File.ReadAllLines("input.txt");
 
-var szamok = new List<int>();
-foreach (var sor in sorok)
+//var szamok = new List<int>();
+/*foreach (var sor in sorok)
 {
     if (sor.Length == 0)
     {
@@ -9,7 +9,83 @@ foreach (var sor in sorok)
     }
     int szam = int.Parse(sor);
     szamok.Add(szam);
+}*/
+
+var dverger = new List<int>();
+//int AntallDverger = 0;
+int AktuellDvergMat = 0;
+
+foreach (var sor in sorok)
+{
+    if (sor.Length == 0)
+    {
+        //AntallDverger++;
+        dverger.Add(AktuellDvergMat);
+        AktuellDvergMat = 0;
+        continue;
+    }
+    int Mat = int.Parse(sor);
+    AktuellDvergMat = AktuellDvergMat + Mat;
+
 }
+if (AktuellDvergMat != 0)
+{
+    dverger.Add(AktuellDvergMat);
+}
+
+var StorDverg = new List<int>();
+StorDverg.Add(0);
+StorDverg.Add(0);
+StorDverg.Add(0);
+int DvergNummer = 0;
+int n = 0;
+int n2 = 0;
+int n3 = 0;
+
+foreach (int dverg in dverger)
+{
+    n++;
+    if (dverg > StorDverg[0])
+    {
+        StorDverg[0] = dverg;
+        DvergNummer = n;
+        n2 = n;
+    }
+
+}
+
+
+n = 0;
+foreach (int Dverg in dverger)
+{
+    n++;
+    if (Dverg > StorDverg[1] && n != n2)
+    {
+        StorDverg[1] = Dverg;
+        DvergNummer = n;
+        n3 = n;
+    }
+
+}
+
+n = 0;
+foreach (int Dverg in dverger)
+{
+    n++;
+    if (Dverg > StorDverg[2] && n != n3 && n != n2)
+    {
+        StorDverg[2] = Dverg;
+        //DvergNummer = n;
+    }
+
+}
+
+
+Console.WriteLine("1st" + StorDverg[0]);
+Console.WriteLine("2nd" + StorDverg[1]);
+Console.WriteLine("3rd" + StorDverg[2]);
+Console.WriteLine("Sum" + (StorDverg[2]+ StorDverg[1]+ StorDverg[0]));
+
 
 
 /*
@@ -25,5 +101,10 @@ foreach (string elem in lista)
 for (int i = 0; i < lista.Count; i++)
 {
     Console.WriteLine(lista[i]);
+}
+
+while (true)
+{
+    break;
 }
 */
