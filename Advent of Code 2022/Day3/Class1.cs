74ab0767c56ab77b06190afb10ad821f36213823
gsc398 <gsc398@gmail.com>
2023-01-10 23:28:09 +0100

    Day3 First Part

---
+++ Advent of Code 2022/Day3/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class MyStringAccessor 
+    {
+        public string FileName;
+
+        public MyStringAccessor(string fileNameLocal)
+        {
+            FileName= fileNameLocal;
+        }
+
+        public int NumberOfBackpacks
+        {
+            get
+            {
+                return File.ReadLines(FileName).Count();
+            }
+            set { }
+        }
+
+        public string FirstCompartment (int index)
+        {
+            string backPackContents = File.ReadLines(FileName).Skip(index).First();
+            string toReturn = new string(backPackContents.Chunk(backPackContents.Length / 2).First());
+            return toReturn;
+        }
+
+        public string SecondCompartment(int index)
+        {
+            string backPackContents = File.ReadLines(FileName).Skip(index).First();
+            string toReturn = new string(backPackContents.Chunk(backPackContents.Length / 2).Last());
+            return toReturn;
+        }
+
+    }
+}
---
+++ Advent of Code 2022/Day3/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day3;
+
+
+//Global
+string fileName = new("ex.txt");
+
+
+MyStringAccessor MyAccessor = new MyStringAccessor(fileName);
+
+Console.WriteLine("Number of backpacks: "+MyAccessor.NumberOfBackpacks);
+
+string repeatedItemList = "";
+for (int currentBackPack = 0; currentBackPack < MyAccessor.NumberOfBackpacks; currentBackPack++)
+{
+    foreach (char itemToTest in MyAccessor.FirstCompartment(currentBackPack))
+    {
+        if (MyAccessor.SecondCompartment(currentBackPack).Contains(itemToTest)) { repeatedItemList += itemToTest; break; }
+    }
+}
+
+int prioritySum = 0;
+foreach(char itemToPrioritize in repeatedItemList)
+{
+    if (itemToPrioritize > 96)
+        prioritySum += itemToPrioritize - 96;
+    else
+        prioritySum += itemToPrioritize - 64 + 26;
+}
+
+Console.WriteLine("Sum priority = " + prioritySum);
+
+//Test section
+/*while(true)
+{
+    Console.WriteLine("Which backpack? ");
+    var inputNumber = Console.ReadLine();
+    Console.WriteLine("Contents in backpack " + inputNumber + " are: " + MyAccessor.FirstCompartment(Convert.ToInt32(inputNumber)));
+    Console.WriteLine("Contents in backpack " + inputNumber + " are: " + MyAccessor.SecondCompartment(Convert.ToInt32(inputNumber)));
+}*/
+
+//Console.WriteLine("Repeated Items List: "+repeatedItemList);
+
+/*for (int n = 97; n < (97+26); n++)
+{
+    char temp = (char)n;
+    Console.WriteLine(temp);
+}*/
+
+
+
+
