792994dc536dfe43badffc0003691ee016ad5c03
gsc398 <gsc398@gmail.com>
2023-02-20 20:04:13 +0100

    Itt hibas

---
+++ Advent of Code 2022/Day5/CrateMover.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class CrateMover
+    {
+        private readonly List<string> instructionsInClass = new List<string>();
+        public CrateMover(List<string> instr) 
+        { 
+            foreach(string line in instr) 
+            {
+                instructionsInClass.Add(line);
+            }
+        }
+
+        public int Count()
+        { return instructionsInClass.Count; }
+
+        public int NextSourceStack(int position)
+        {
+            string currentLine = instructionsInClass[position];
+            var numberAsString = new StringBuilder();
+            int stringPosition = 5; //Set the position in the string (current line of instruction) to 5, where the first digit of the first data is located
+            stringPosition = stringPosition + this.NumberToMove(position).ToString().Length; //...es most hozzaadom hany jegyu az elso adat mezo
+            stringPosition = stringPosition + 6; //...es ez eljuttat a masodik adatmezo kezdeteig. Azert irtam ki 3 sorban, hogy jobban ertheto/olvashato legyen
+            while (true)
+            {
+                numberAsString.Append(currentLine[stringPosition]);
+                stringPosition++;
+                if (currentLine[stringPosition] == ' ') { break; }
+            }
+            return (int)Int16.Parse(numberAsString.ToString());
+        }
+
+        public int NextDestinationStack(int position)
+        {
+            string currentLine = instructionsInClass[position];
+            var numberAsString = new StringBuilder();
+            int stringPosition = 5; //Set the position in the string (current line of instruction) to 5, where the first digit of the first data is located
+            stringPosition = stringPosition + this.NumberToMove(position).ToString().Length; //...es most hozzaadom hany jegyu az elso adat mezo
+            stringPosition = stringPosition + 6; //...es ez eljuttat a masodik adatmezo kezdeteig. Azert irtam ki 3 sorban, hogy jobban ertheto/olvashato legyen
+            stringPosition = stringPosition + this.NextSourceStack(position).ToString().Length + 4; //...es ez az elozo alapjan eljuttat at utolso adatmezohoz
+            while (true)
+            {
+                numberAsString.Append(currentLine[stringPosition]);
+                stringPosition++;
+                if (stringPosition == currentLine.Length - 1) { break; }
+            }
+            return (int)Int16.Parse(numberAsString.ToString());
+        }
+
+        public int NumberToMove(int position)
+        {
+            string currentLine = instructionsInClass[position];
+            var numberAsString = new StringBuilder();
+            int stringPosition = 5; //Set the position in the string (current line of instruction) to 5, where the first digit of the data in question is located
+            while (true)
+            {
+                numberAsString.Append(currentLine[stringPosition]);
+                stringPosition++;
+                if (currentLine[stringPosition] == ' ')
+                { break; }
+
+            } 
+,           return (int)Int16.Parse(numberAsString.ToString());
+        }
+    }
+}
--- Advent of Code 2022/Day5/Program.cs
+++ Advent of Code 2022/Day5/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Split the input file into 2 sets of lines with the logically different content (stacks and instructions)
+using Day5;
 using System.Security.Cryptography.X509Certificates;
 
-var fileContents = File.ReadAllLines("ex.txt");
+var fileContents = File.ReadAllLines("input.txt");
 List<string> stacks = new List<string>();
 List<string> instructions = new List<string>();
 bool emptyLineReached =false;
@@ -22,7 +23,7 @@ foreach(string line in fileContents)
 }
 
 //Testing if it worked
-foreach (string line in stacks)
+/*foreach (string line in stacks)
 {
     Console.WriteLine(line);
 }
@@ -30,7 +31,7 @@ Console.WriteLine(" Next Set ");
 foreach(string line in instructions)
 {
     Console.WriteLine(line);
-}
+}*/
 //Mukodott. :) (Na, nem elsore...)
 
 //Most a kupacokkal foglalkozok, levagom az also sort, mert azok csak szamok, es megallapitom hany kupac van
@@ -48,7 +49,7 @@ for (int n=lastLineOfStacks.Length; n > 0; n--)
 }
 string nrOfStacksTemporaryString = new string(nrOfStacksTemporaryChar);
 //Console.WriteLine("Number of Stacks:" +nrOfStacksTemporaryString);
-nrOfStacksTemporaryString=nrOfStacksTemporaryString.Replace((char)0,' ');
+nrOfStacksTemporaryString = nrOfStacksTemporaryString.Replace((char)0, ' ');
 nrOfStacksTemporaryString = nrOfStacksTemporaryString.Trim();
 int nrOfStacks = (int) Int64.Parse(nrOfStacksTemporaryString);
 Console.WriteLine("Number of Stacks:" + nrOfStacks.ToString());
@@ -59,23 +60,52 @@ stacks.RemoveAt(stacks.Count - 1); //Kivettem a listabol az utolso sort, ami nem
 //A Stack abrazolasaban fix karakterpoziciokban van a crate neve, tehat csak ezeket a karakterpoziciokat kell nezni. 
 //Ami megbonyolitja, hogy nem minden sor feltetlenul ugyanolyan hosszu, tehat nem minden pozicio letezik.
 
+//List<char>[] stackList = new List<char>[nrOfStacks]; //Ez a sor eddig a legeslegnehezebb felfogni meg a Te segitsegeddel is! Brutal ez a c# logika! Nem embernek valo!
+//Tovabbra is teljesen bizonytalan vagyok, hiaba irtad le, mert azt sem igazan ertem, hogy ez most tulajdonkeppen mit is hoz letre?? Te azt mondod tombot. De akkor milyen tipusu 
+//tombot? ...es ha egyszer az van ott, hogy List, akkor miert nem listat hoz letre? :o 
+
+//Javaslod itt ezt a StringBuildert, dehat hogy kell azt hasznali????? En nem merem. Maradok a listaknal, igazabol azt sem tudom meg hasznalni. 
+
 List<char>[] stackList = new List<char>[nrOfStacks];
+for (int n=0; n < nrOfStacks; n++)
+{ stackList[n] = new List<char>(); }
+
 foreach (string line in stacks)
 {
     for (int selectedStack = 0; selectedStack < nrOfStacks; selectedStack++)
     {
-        try
+        if ((1 + (4 * selectedStack)) <= (line.Length - 1))
         {
-            if (line[1+(3*selectedStack)] != ' ') //Megadja (remelem) a kovetkezo poziciot olvasasra
+            if (line[1 + (4 * selectedStack)] != ' ') //Megadja (remelem) a kovetkezo poziciot olvasasra
             {
-                stackList[selectedStack].Add(line[1 + (3 * selectedStack)]);
+                stackList[selectedStack].Add(line[1 + (4 * selectedStack)]);
             }
         }
-        catch
-        {
-            break;
-        }
+        
     }
 }
 
 //Ellenorzes
+//Megnéztem a debuggerben a változók tartalmár és jó.
+
+//Akkor most johet maga a feladat, a dobozok mozgatasa.... El kell olvasnom ujra a feladatot, mar nem emlekszem. 
+
+var DataProcessor = new CrateMover(instructions);
+
+//int nextSourceStack;
+//int nextDestinationStack;
+//int numberToMove;
+
+for (int position = 0; position < DataProcessor.Count(); position++)
+{
+    for (int moveCounter = 0; moveCounter < DataProcessor.NumberToMove(position); moveCounter++)
+    {
+        //nextDestinationStack = DataProcessor.NextDestinationStack(position);
+        //nextSourceStack = DataProcessor.NextSourceStack(position);
+        Console.WriteLine(DataProcessor.NumberToMove(position).ToString() + " " + DataProcessor.NextSourceStack(position).ToString() + " " + DataProcessor.NextDestinationStack(position).ToString());
+    }
+    // Ellenorzes: Passed // Console.WriteLine(DataProcessor.NumberToMove(position).ToString() ...... ilyesmi.
+    
+
+}
+
